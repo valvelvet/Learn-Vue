ddbb2f80efc78aef01a7b81eb68c076ae980eb88
Annie <anniewang@softmobile.com.tw>
2022-10-05 14:53:21 +0800

    Vue-Learn-pratice friendList with vue CLI

--- learn-pratice/src/App.vue
+++ learn-pratice/src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <article>
+    <header>
+      <h1>Friends List</h1>
+    </header>
+    <ul>
+      <friend-conact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone="friend.phone"
+        :email="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavorite"
+      >
+      </friend-conact>
+    </ul>
+  </article>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Amy",
+          phone: "09-8765 4321",
+          email: "amy001@fake.mail.com",
+        },
+        {
+          id: 2,
+          name: "Bill",
+          phone: "01-2345 6789",
+          email: "bill002@fake.mail.com",
+        },
+        {
+          id: 3,
+          name: "Cali",
+          phone: "01-2345 6789",
+          email: "cali003@fake.mail.com",
+          isFavorite: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavorite(friendId) {
+      const friend = this.friends.find((friend) => friend.id === friendId);
+      friend.isFavorite = !friend.isFavorite;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 90%;
+  max-width: 40rem;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 </style>
--- learn-pratice/src/main.js
+++ learn-pratice/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import FriendConact from "./components/FriendConact.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("friend-conact", FriendConact);
+app.mount("#app");
