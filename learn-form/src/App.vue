7011a126185ef79ab500259b309ab82818669d1b
Annie <anniewang@softmobile.com.tw>
2022-10-11 17:24:48 +0800

    Vue-Learn-Vue form

---
+++ learn-form/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  color: #555;
+  box-sizing: border-box;
+  list-style: none;
+}
+</style>
--- learn-pratice/src/components/ManageGoals.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>Manage Goals</h2>
-</template>
