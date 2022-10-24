a97436979c1ce9f67e1af83a7a550a272b693df4
Annie <anniewang@softmobile.com.tw>
2022-10-24 09:29:32 +0800

    Vue-Learn-Vuex

--- learn-vuex/src/App.vue
+++ learn-vuex/src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h2>{{ count }}</h2>
+    <h2>{{ rangeCount }}</h2>
+    <button @click="addCount">+</button>
+  </base-container>
 </template>
 
 <script>
-import BaseContainer from './components/BaseContainer.vue';
+import BaseContainer from "./components/BaseContainer.vue";
 
 export default {
   components: {
     BaseContainer,
+  },
+  computed: {
+    count() {
+      return this.$store.state.counter;
+    },
+    rangeCount(){
+      return this.$store.getters.rangeCounter;
+    }
+  },
+  methods: {
+    addCount() {
+      // 使用 mutations的兩種寫法
+      // - 1
+      // this.$store.commit("addCounter", { num: 10 });
+      // - 2
+      this.$store.commit({
+        type: "addCounter",
+        num: 10,
+      });
+    },
   },
 };
 </script>
--- learn-vuex/src/main.js
+++ learn-vuex/src/main.js
@@ -1,7 +1,36 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import { createStore } from "vuex";
 
-import App from './App.vue';
+import App from "./App.vue";
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    addCounter(state, payload) {
+      // state：預設參數，指向 store的 state
+      // payload：可選參數，接收額外傳入的資料，傳物件可以包涵更多資訊
+      state.counter += payload.num;
+      // console.log(this.state.counter);
+      // console.log(state.counter);
+    },
+  },
+  getters: {
+    doubleCounter(state) {
+      return state.counter * 2;
+    },
+    rangeCounter(_state, getter) {
+      if (getter.doubleCounter > 100) return 100;
+      else return getter.doubleCounter;
+    },
+  },
+});
 
 const app = createApp(App);
 
-app.mount('#app');
+app.use(store);
+
+app.mount("#app");
